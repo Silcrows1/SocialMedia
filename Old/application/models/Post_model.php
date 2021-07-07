62fd5ed16a301b2050327f2d378a237052066e18
Silcrows1 <Silcrows@hotmail.com>
2021-07-07 15:08:05 +0100

    Friends database call
    Altered the way in which friends posts are retrieved to prevent unnecessary calls to the database. Rather than calling for each friend, it is now performed using the user_id of the session holder

--- Old/application/models/Post_model.php
+++ Old/application/models/Post_model.php
@@ -15,54 +15,44 @@ class Post_model extends CI_model{
 
         
     }
+    //Find friends function
+    public function friends(){
+        $this->db->select('Usertwo_id');
+        $this->db->from('friends');
+        $this->db->where('User_id', $this->session->userdata('user_id'));
+        $friends=$this->db->get();    
+        return $friends->result_array();
 
+         
+    }
+    //view posts function
     public function viewPosts(){
 
+            //SQL call for all posts that match session id in friends database
 
-        //SELECT `FirstName`, `LastName`, `posts`.`Content` FROM `users`
-        //JOIN `profiles` ON `users`.`User_id` = `profiles`.`User_id`
-        //JOIN `posts` ON `posts`.`User_id` = `profiles`.`User_id`
-        //WHERE `users`.`User_id` = '25'
+                $this->db->select('users.FirstName, users.LastName, posts.Content, users.user_id, posts.Posted, friends.Usertwo_id');
+                $this->db->from('friends');
+                $this->db->join('users', 'users.User_id = friends.Usertwo_id');
+                $this->db->join('profiles', 'users.User_id = profiles.User_id');
+                $this->db->join('posts', 'posts.User_id = profiles.User_id');
+                
+                $this->db->where('friends.User_id = '. $this->session->userdata('user_id'));
+                $this->db->order_by('posts.Posted', 'DESC');
+                $posts = $this->db->get();
+                $posts->result_array();
 
-        //SELECT `users`.`FirstName`, `users`.`LastName`, `posts`.`Content` 
-        //JOIN `profiles` ON `users`.`User_id` = `profiles`.`User_id` 
-        //JOIN `posts` ON `posts`.`User_id` = `profiles`.`User_id` 
-        //WHERE `users`.`User_id` = '25'
-
-        $this->db->select('users.FirstName, users.LastName, posts.Content, users.user_id, posts.Posted');
-        $this->db->from('users');
-        $this->db->join('profiles', 'users.User_id = profiles.User_id');
-        $this->db->join('posts', 'posts.User_id = profiles.User_id');
-        $this->db->order_by('posts.Posted', 'DESC');
-
-        //$this->db->join('posts', 'profiles.Users_id = posts.User_id');
-        //$this->db->where('users.User_id', $this->session->userdata('user_id'));
-        //$user = $this->db->get('users');
-        $posts=$this->db->get();
-        return $posts->result_array();
+            return $posts->result_array();
+                
     }
 
     public function viewaPost($id){
-
-
-        //SELECT `FirstName`, `LastName`, `posts`.`Content` FROM `users`
-        //JOIN `profiles` ON `users`.`User_id` = `profiles`.`User_id`
-        //JOIN `posts` ON `posts`.`User_id` = `profiles`.`User_id`
-        //WHERE `users`.`User_id` = '25'
-
-        //SELECT `users`.`FirstName`, `users`.`LastName`, `posts`.`Content` 
-        //JOIN `profiles` ON `users`.`User_id` = `profiles`.`User_id` 
-        //JOIN `posts` ON `posts`.`User_id` = `profiles`.`User_id` 
-        //WHERE `users`.`User_id` = '25'
 
         $this->db->select('users.FirstName, users.LastName, posts.Content, posts.Posted');
         $this->db->from('users');
         $this->db->join('profiles', 'users.User_id = profiles.User_id');
         $this->db->join('posts', 'posts.User_id = profiles.User_id');
-        //$this->db->join('posts', 'profiles.Users_id = posts.User_id');
         $this->db->where('users.User_id', $id);
         $this->db->order_by('posts.Posted', 'DESC');
-        //$user = $this->db->get('users');
         $posts=$this->db->get();
         return $posts->result_array();
     }
